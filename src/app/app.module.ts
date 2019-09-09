00b411c00c083d8a46670e9de62f3dbd99c7b218
Katarzyna <katstudz@gmail.com>
2019-09-09 12:28:25 +0200

    add heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
+  // to co chcemy uzywac
   imports: [
     BrowserModule
   ],
+  // to co chcemy reuzywac
   providers: [],
   bootstrap: [AppComponent]
 })
